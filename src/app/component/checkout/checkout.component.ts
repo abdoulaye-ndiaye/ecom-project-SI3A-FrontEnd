ec07d4882de89d71996374b7ad0e74dc5974663d
abdoulaye-ndiaye <wizlaye7@gmail.com>
2025-01-11 19:07:15 +0100

    good

--- src/app/component/checkout/checkout.component.ts
+++ src/app/component/checkout/checkout.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -22,7 +23,7 @@ export class CheckoutComponent implements OnInit {
   paymentMethod: string = '';
   userId: string = '';
 
-  constructor(private cartService: CartService, private fb: FormBuilder, private OrdersService: OrdersService, private UsersService: UsersService) {
+  constructor(private cartService: CartService, private fb: FormBuilder, private OrdersService: OrdersService, private UsersService: UsersService, private router: Router) {
     // Initialize billing form with validation
     this.billingForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2), Validators.pattern(/^[a-zA-Z]+$/)]],
@@ -66,94 +67,94 @@ export class CheckoutComponent implements OnInit {
       : { mismatch: true };
   }
 
-  placeOrder(): void {
+  async placeOrder(): Promise<void> {
     if (this.billingForm.invalid) {
       console.error('Please fill out the form correctly before placing an order.');
-      return; // Exit if the form is invalid
-    }
-    // creer un utilisateur
-    const userDetails = {
-      prenom: this.billingForm.value.firstName,
-      nom: this.billingForm.value.lastName,
-      email: this.billingForm.value.email,
-      numTel: this.billingForm.value.mobile,
-      adresse: this.billingForm.value.address1,
-      password: this.billingForm.value.password,
-      role:'USER',
-    };
-    this.UsersService.createUser(userDetails).subscribe(
-      (response: any) => {
-        console.log('User successfully created:', response);
-        Swal.fire({
-          icon: 'success',
-          title: 'Compte cree avec succéss',
-          showConfirmButton: false,
-          timer: 1200,
-        });
-      },
-      (error: any) => {
-        console.error('Error creating user:', error);
-      }
-    );
-
-    // connecter l'utilisateur
-    const credentials = {
-      email: this.billingForm.value.email,
-      password: this.billingForm.value.password,
-    };
-
-    this.UsersService.login(credentials).subscribe(
-      (response: any) => {
-        console.log('User successfully logged in:', response);
-        localStorage.setItem('token', response.token);
-      },
-      (error: any) => {
-        console.error('Error logging in user:', error);
-      }
-    );
-
-    // recuperer l'id de l'utilisateur sur le token
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken = jwtDecode<{ user: { _id: string } }>(token);
-      console.log('Decoded token:', decodedToken);
-      this.userId = decodedToken.user._id;
-      console.log('User ID:', this.userId);
-    } else {
-      console.error('No token found in localStorage');
+      return;
     }
 
-  
-    // Préparer les détails de la commande
-    const orderDetails = {
-      user: this.userId, // Remplacez par l'ID de l'utilisateur authentifié
-      status: 'pending', // Statut initial de la commande
-      products: this.cartItems.map(item => ({
-        product: item.product._id,
-        quantity: item.quantity,
-      })),
-      address: this.billingForm.value.address1 || this.billingForm.value.address2, // Adresse de facturation
-      paymentMethod: this.paymentMethod, // Méthode de paiement sélectionnée
-    };
-  
-    // Appel de l'API pour créer une commande
-    this.OrdersService.createOrder(orderDetails).subscribe(
-      (response: any) => {
-        console.log('Order successfully created:', response);
-        Swal.fire({
-          icon: 'success',
-          title: 'Commande cree avec succéss',
-          showConfirmButton: false,
-          timer: 1200,
-        });
-        // Vider le panier après la commande
-        this.cartService.clearCart();
-      },
-      (error: any) => {
-        console.error('Error creating order:', error);
+    try {
+      // 1. Créer un utilisateur
+      const userDetails = {
+        prenom: this.billingForm.value.firstName,
+        nom: this.billingForm.value.lastName,
+        email: this.billingForm.value.email,
+        numTel: this.billingForm.value.mobile,
+        adresse: this.billingForm.value.address1,
+        password: this.billingForm.value.password,
+        role: 'USER',
+      };
+      const createUserResponse = await this.UsersService.createUser(userDetails).toPromise();
+      console.log('User successfully created:', createUserResponse);
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Compte créé avec succès',
+        showConfirmButton: false,
+        timer: 1200,
+      });
+
+      // 2. Connecter l'utilisateur
+      const credentials = {
+        email: this.billingForm.value.email,
+        password: this.billingForm.value.password,
+      };
+      
+      const loginResponse = await this.UsersService.login(credentials).toPromise();
+      console.log('User successfully logged in:', loginResponse);
+      // suppression du token dans le local storage
+      localStorage.removeItem('token');
+      // Stocker le token
+      localStorage.setItem('token', loginResponse.token);
+
+      // 3. Décoder le token pour récupérer l'ID de l'utilisateur
+      const token = localStorage.getItem('token');
+      if (token) {
+        const decodedToken = jwtDecode<{ user: { _id: string } }>(token);
+        this.userId = decodedToken.user._id;
+        console.log('User ID:', this.userId);
+      } else {
+        console.error('No token found in localStorage');
+        return;
       }
-    );
+
+      // 4. Préparer les détails de la commande
+      const orderDetails = {
+        user: this.userId,
+        status: 'PENDING',
+        products: this.cartItems.map(item => ({
+          product: item.product._id,
+          quantity: item.quantity,
+        })),
+        address: this.billingForm.value.address1 || this.billingForm.value.address2,
+        paymentMethod: this.paymentMethod,
+      };
+
+      // 5. Créer la commande
+      const createOrderResponse = await this.OrdersService.createOrder(orderDetails).toPromise();
+      console.log('Order successfully created:', createOrderResponse);
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Commande créée avec succès. Merci de bien verifier votre email!',
+        showConfirmButton: false,
+        timer: 2200,
+      }).then(() => {
+        this.router.navigate(['/dashboard']);
+      });
+
+      // 6. Vider le panier
+      this.cartService.clearCart();
+     
+
+    } catch (error) {
+      console.error('Error during order process:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Erreur',
+        text: 'Une erreur s\'est produite lors du traitement de votre commande.',
+      });
+    }
   }
-  
 }
 
